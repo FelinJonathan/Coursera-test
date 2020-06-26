2fb01ae3dab514e0b993f0caec28dce13d19eb90
Your Name <you@example.com>
2020-06-26 09:48:11 +0530

    app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
   'use strict';
 
   angular.module('myFirstApp', []);
 
-  .controller('MyFirstController', function($scope)){
+  .controller('MyFirstController', function($scope){
 
 
   });
